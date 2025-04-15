4bc329a7198b62e3a992974d097dc3e3d17e3ddf
SrRobot1999 <samuelbereche1999@gmail.com>
2025-04-14 22:10:55 -0500

    primer commit prueba01

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,7 @@
+<nav class="bg-gray-800 text-white p-2.5">
+    <ul class="list-none flex gap-4 m-0 p-0">
+        <li><a href="#" class="text-white no-underline hover:underline">Mi perfil</a></li>
+        <li><a href="#" class="text-white no-underline hover:underline">Notificaciones</a></li>
+        <li><a href="#" class="text-white no-underline hover:underline">Mensajes</a></li>
+    </ul>
+</nav>
---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,7 @@
+<aside style="width: 200px; background-color: #f4f4f4; padding: 15px; height: 100vh;">
+    <ul style="list-style: none; padding: 0;">
+        <li><a href="#" style="text-decoration: none; color: #333;">Dashboard</a></li>
+        <li><a href="#" style="text-decoration: none; color: #333;">Perfil</a></li>
+        <li><a href="#" style="text-decoration: none; color: #333;">Configuración</a></li>
+    </ul>
+</aside>
